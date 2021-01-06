4cb69834ebe96de7cca3ecf82ed8474e12df8001
ChrisChrisLoLo <sporewoh@gmail.com>
2021-01-06 03:19:14 -0700

    Finish mapping game state to canvas

--- src/features/game/GameCanvas.tsx
+++ src/features/game/GameCanvas.tsx
@@ -4,8 +4,7 @@ import { Stage, Sprite, Container } from '@inlet/react-pixi';
 import { settings, SCALE_MODES} from 'pixi.js';
 import { connect, ConnectedProps, RootStateOrAny } from 'react-redux';
 import { LevelData } from '../../logic/game/objects/LevelData';
-
-import sprites from './assets/turtl/sprites';
+import { sprites, tileSpriteEnumMap, turtleAngleDirMap } from './assets/turtl/sprites';
 
 // Disable interpolation when scaling, will make texture be pixelated
 settings.SCALE_MODE = SCALE_MODES.NEAREST;
@@ -29,7 +28,9 @@ interface Props extends PropsFromRedux {}
 const STAGE_WIDTH: number = 1200
 const STAGE_HEIGHT: number = 800
 
-const TILE_WIDTH: number = 64
+const SPRITE_IMAGE_PX: number = 16
+const TILE_SIZE_PX: number = 64
+const TILE_SCALE: number = TILE_SIZE_PX/SPRITE_IMAGE_PX
 
 const CONTAINER_PADDING_X = 50
 const CONTAINER_PADDING_Y = 50
@@ -45,20 +46,54 @@ function GameCanvas(props:Props) {
   }
   const levelWidth: number = props.level[0].length
 
-  // const tileWidth = Math.floor(Math.min(STAGE_HEIGHT,STAGE_WIDTH)/Math.max(levelHeight,levelWidth))
+  const tileSprites = props.level.map((row,i) => {
+    return row.map((tile,j) => {
+      return <Sprite
+        image = {tileSpriteEnumMap.get(tile)}
+        scale = {TILE_SCALE}
+        x = {i*TILE_SIZE_PX}
+        y = {j*TILE_SIZE_PX}
+        zIndex = {0}
+        key = {`${i},${j}`}
+      />
+    })
+  })
 
+  const flagSprites = props.flags ? props.flags.map(flag => 
+    <Sprite
+      image = {sprites.flag}
+      scale = {TILE_SCALE}
+      x={flag.pos.x*TILE_SIZE_PX}
+      y={flag.pos.y*TILE_SIZE_PX}
+      zIndex = {5}
+      key = {`${flag.pos.x},${flag.pos.y}`}
+    />
+  ) : null;
+
+  console.log(props.flags)
+  console.log(flagSprites)
+
+  // pivot is required to rotate sprite around the center
+  // pivot is placed in the center of the sprite before scaling
+  // because pivot causes a offset in the sprite, a readjustment needs to be made to the x and y
+  const turtleSprite = <Sprite
+    image = {sprites.turtle}
+    scale = {TILE_SCALE}
+    x={(props.player.pos.x*TILE_SIZE_PX)+TILE_SIZE_PX/2}
+    y={(props.player.pos.y*TILE_SIZE_PX)+TILE_SIZE_PX/2}
+    pivot={[SPRITE_IMAGE_PX/2,SPRITE_IMAGE_PX/2]}
+    angle={turtleAngleDirMap.get(props.player.dir)}
+    zIndex = {10}
+  />              
+            
   return (
     <div>
       <Stage width={STAGE_WIDTH} height={STAGE_HEIGHT} style={{ width: '100%' }} options={{ backgroundColor: 0xeef1f5,  }}>
         <Container position={[CONTAINER_PADDING_X,CONTAINER_PADDING_Y]}>
-          <Sprite
-            image= {sprites.turtle}
-            scale = {4}
-            // height={TILE_WIDTH}
-            // width={TILE_WIDTH}
-            x={0}
-            y={0}
-          />
+          {tileSprites}
+          {flagSprites}
+          {turtleSprite}
+          
         </Container>
       </Stage>
     </div>
--- src/features/game/assets/turtl/sprites.ts
+++ src/features/game/assets/turtl/sprites.ts
@@ -1,11 +1,13 @@
 import turtle from './turtle.png'
-import flag from './flag.png';
-import tileBlack from './tile_black.png';
-import tileBlue from './tile_blue.png';
-import tileEmpty from './tile_empty.png';
-import tileWhite from './tile_white.png';
+import flag from './flag.png'
+import tileBlack from './tile_black.png'
+import tileBlue from './tile_blue.png'
+import tileEmpty from './tile_empty.png'
+import tileWhite from './tile_white.png'
+import { TileType } from '../../../../logic/game/objects/TileType'
+import { DirectionType } from '../../../../logic/game/objects/Directions'
 
-export default {
+export const sprites = {
   turtle: turtle,
   flag: flag,
   tileBlack: tileBlack,
@@ -13,3 +15,17 @@ export default {
   tileEmpty: tileEmpty,
   tileWhite: tileWhite
 }
+
+export const tileSpriteEnumMap = new Map<TileType, string>([
+  [TileType.BLACK, tileBlack],
+  [TileType.BLUE, tileBlue],
+  [TileType.EMPTY, tileEmpty],
+  [TileType.WHITE, tileWhite],
+])
+
+export const turtleAngleDirMap = new Map<DirectionType, number>([
+  [DirectionType.NORTH, 180],
+  [DirectionType.EAST, 270],
+  [DirectionType.SOUTH, 0],
+  [DirectionType.WEST, 90],
+])
