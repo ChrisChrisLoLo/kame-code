3b037c1ea30da5723c0ebfe768abcccf459e4a5b
ChrisChrisLoLo <sporewoh@gmail.com>
2021-01-16 06:03:22 -0700

    create isPlaybackDisabled function

--- src/features/game/GamePlayer.tsx
+++ src/features/game/GamePlayer.tsx
@@ -41,6 +41,10 @@ function GamePlayer(props:Props) {
     props.setIsPlaybackOn(!props.isPlaybackOn)
   }
 
+  function isPlaybackDisabled(){
+    return props.playbackQueue.length===0
+  }
+
   useEffect(() => {
     console.log('init')
     const timeout = setTimeout(() => {
@@ -77,8 +81,6 @@ function GamePlayer(props:Props) {
     </svg>
   }
 
-  console.log(props.playbackQueue.length===0)
-
   return (
     <div>
       <GameCanvas 
@@ -96,13 +98,14 @@ function GamePlayer(props:Props) {
         min={0}
         max={props.playbackQueue.length-1} 
         value={props.playbackIndex}
+        disabled={isPlaybackDisabled()}
       />
 
       <div>
         <button 
           className="bg-green-500 hover:bg-green-600 disabled:bg-gray-600 disabled:opacity-40 text-white hover:text-gray-100 mr-1 w-7 rounded"
           onClick={()=>stepBack()}
-          disabled={props.playbackQueue.length===0}
+          disabled={isPlaybackDisabled()}
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0019 16V8a1 1 0 00-1.6-.8l-5.333 4zM4.066 11.2a1 1 0 000 1.6l5.334 4A1 1 0 0011 16V8a1 1 0 00-1.6-.8l-5.334 4z" />
@@ -112,7 +115,7 @@ function GamePlayer(props:Props) {
         <button 
           className="bg-green-500 hover:bg-green-600 disabled:bg-gray-600 disabled:opacity-40 text-white hover:text-gray-100 mr-1 w-7 rounded"
           onClick={()=>toggleIsPlaybackOn()}
-          disabled={props.playbackQueue.length===0}
+          disabled={isPlaybackDisabled()}
         >
           {playPauseIcon}
         </button>
@@ -120,14 +123,13 @@ function GamePlayer(props:Props) {
         <button
           className="bg-green-500 hover:bg-green-600 disabled:bg-gray-600 disabled:opacity-40 text-white hover:text-gray-100 w-7 rounded"
           onClick={()=>stepForward()}
-          disabled={props.playbackQueue.length===0}
+          disabled={isPlaybackDisabled()}
         >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11.933 12.8a1 1 0 000-1.6L6.6 7.2A1 1 0 005 8v8a1 1 0 001.6.8l5.333-4zM19.933 12.8a1 1 0 000-1.6l-5.333-4A1 1 0 0013 8v8a1 1 0 001.6.8l5.333-4z" />
           </svg>
         </button>
       </div>
-      {/* {hasUserWonEl} */}
     </div> 
   )
 }
